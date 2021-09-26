2a36862a5db96fded350956e9369fc16beae1ddb
Jaime López <loama18@gmail.com>
2021-09-25 19:15:14 -0500

    topbar

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,15 @@
     <LoadingPage />
     <Onboarding />
 
-    <router-view/>
+    <div class="font-teko-regular h-14 leading-16 text-center text-2xl uppercase"
+      id="topbar">
+      <img class="align-top inline mt-5 w-6" src="@/assets/logo.svg" />
+      MarketPryce
+    </div>
+
+    <div id="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -51,4 +59,11 @@ export default {
 
 html, body
   @apply bg-black
+
+#app
+  padding-top: env(safe-area-inset-top)
+
+#topbar
+  border-bottom: 0.5px solid rgba(122, 126, 128, 0.5)
+  border-top: 0.5px solid rgba(122, 126, 128, 0.5)
 </style>
